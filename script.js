83e377dac9b5cf7b5072b605af2662dcf6a0e290
MateoDedi <mateod@live.com>
2023-10-12 14:10:52 +0200

    add

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-const img1 = document.querySelector(".img1");
-const img2 = document.querySelector(".img2");
-const img3 = document.querySelector(".img3");
-const img4 = document.querySelector(".img4");
-const img5 = document.querySelector(".img5");
-const img6 = document.querySelector(".img6");
-const img7 = document.querySelector(".img7");
-const img8 = document.querySelector(".img8");
-const img9 = document.querySelector(".img9");
-
-document.addEventListener(`DOMContentLoaded`, function() {
-
+document.addEventListener('DOMContentLoaded', function () {
     const swiper = new Swiper('.swiper', {
         direction: 'horizontal',
         loop: true,
         slidesPerView: 1,
     });
 
-    img1.addEventListener("click", () => {
-        swiper.slideTo(0);
+    const imageElements = document.querySelectorAll('img');
+    const background = document.body.style;
+
+    imageElements.forEach((image, index) => {
+        image.addEventListener('click', () => {
+            const houseName = getHouseNameFromImage(image);
+            setHouseBackground(houseName);
+
+            swiper.slideTo(index + 1);
+
+            // Reset opacity for all images to 0.4
+            imageElements.forEach((img) => {
+                img.style.opacity = '0.4';
+            });
+
+            // Set opacity to 1 for the clicked image
+            image.style.opacity = '1';
+        });
     });
 
-    img2.addEventListener("click", () => {
-        swiper.slideTo(1);
-    });
+    function getHouseNameFromImage(image) {
+        // Extract the house name from the alt attribute of the image
+        return image.alt;
+    }
 
-    img3.addEventListener("click", () => {
-        swiper.slideTo(2);
-    });
-
-    img4.addEventListener("click", () => {
-        swiper.slideTo(3);
-    });
-
-    img5.addEventListener("click", () => {
-        swiper.slideTo(4);
-    });
-
-    img6.addEventListener("click", () => {
-        swiper.slideTo(5);
-    });
-
-    img7.addEventListener("click", () => {
-        swiper.slideTo(6);
-    });
-
-    img8.addEventListener("click", () => {
-        swiper.slideTo(7);
-    });
-
-    img9.addEventListener("click", () => {
-        swiper.slideTo(8);
-    });
-
+    function setHouseBackground(houseName) {
+        // Define background images for each house
+        const backgroundImages = {
+            Stark: 'backgrounds/Stark.jpg',
+            Lannister: 'backgrounds/lannister.jpg',
+            Targaryen: 'backgrounds/house targaryen.jpg',
+        };
+    
+        // Set the background image based on the house name
+        if (backgroundImages.hasOwnProperty(houseName)) {
+            background.backgroundImage = `url(${backgroundImages[houseName]})`;
+            background.backgroundRepeat = 'no-repeat';
+            background.backgroundSize = 'cover';
+            background.backgroundAttachment = 'fixed';
+        } else {
+            // If the house name is not found, set the background to the default
+            background.backgroundImage = 'url(backgrounds/game_of_thrones_hbo_world_map_by_crlyle_d4vvlyo.jpg)';
+            background.backgroundRepeat = 'no-repeat';
+            background.backgroundSize = 'cover';
+            background.backgroundAttachment = 'fixed';
+        }
+    }    
 });
